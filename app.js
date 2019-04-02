2d5261f693da62f77567e00f4b9d7c6c5cab56e7
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-04-02 14:44:22 +0100

    criamção do server e criação do listening

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// fazendo loading danoss app utilizando express...
+// fazendo loading da app utilizando express...
+const express = require('express')
+const app = express()
+
+//localhost 3003
+app.listen(3003, ()=> {
+    console.log("Servidor rodando e fazendo listening na porta 3003")
+})
